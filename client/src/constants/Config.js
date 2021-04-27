6f8be889d7fbe521fba8e44de5e4c5da6cc3bf78
Mark Makondo <markalbert.makondo@gmail.com>
2021-04-28 01:32:38 +0800

    production options enabled

--- client/src/constants/Config.js
+++ client/src/constants/Config.js
@@ -1,18 +1,18 @@
 export const GOOGLE_CLIENT_ID = '934331962195-0k9qksgpq7j703f84o6ocf6t0unps4ll.apps.googleusercontent.com';
 
 // ENABLE FOR DEVELOPMENT
-// socket connection
-export const SOCKET_OPTIONS = '';
-export const BACKEND_SERVER = 'http://localhost:5000';
-// axios intance
-export const AXIOS_SERVER = 'http://localhost:5000';
+// // socket connection
+// export const SOCKET_OPTIONS = '';
+// export const BACKEND_SERVER = 'http://localhost:5000';
+// // axios intance
+// export const AXIOS_SERVER = 'http://localhost:5000';
 
 // ENABLE FOR PRODUCTION
-// // socket connection
-// export const SOCKET_OPTIONS = {
-// 	// fixes the cors error for socket io
-// 	transports: ['websocket'],
-// };
-// export const BACKEND_SERVER = 'http://104.236.120.68.xip.io:5000 ';
-// // axios intance
-// export const AXIOS_SERVER = '';
+// socket connection
+export const SOCKET_OPTIONS = {
+	// fixes the cors error for socket io
+	transports: ['websocket'],
+};
+export const BACKEND_SERVER = 'http://104.236.120.68.xip.io:5000 ';
+// axios intance
+export const AXIOS_SERVER = '';
